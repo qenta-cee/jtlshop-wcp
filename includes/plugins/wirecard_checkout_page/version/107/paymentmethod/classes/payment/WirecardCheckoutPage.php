d87ff6059fd27d8f8d394ecc709df43c6ddac710
jakubpolomsky <jakub.polomsky@partner.wirecard.com>
2017-06-07 09:59:19 +0200

    #8 add masterpass
    - send no_shipping shipping profile

--- includes/plugins/wirecard_checkout_page/version/107/paymentmethod/classes/payment/WirecardCheckoutPage.php
+++ includes/plugins/wirecard_checkout_page/version/107/paymentmethod/classes/payment/WirecardCheckoutPage.php
@@ -126,6 +126,9 @@ class WirecardCheckoutPage extends PaymentMethod
                 ->createConsumerMerchantCrmId($_SESSION['Kunde']->cMail)
                 ->setWindowName(WIRECARD_CHECKOUT_PAGE_WINDOWNAME);
 
+            if( $this->paymenttype == WirecardCEE_QMore_PaymentType::MASTERPASS )
+                $client->setShippingProfile('NO_SHIPPING');
+
             $client->xIframeUsed = $this->getConfig('use_iframe');
             $client->xLanguage = convertISO2ISO639($_SESSION['cISOSprache']);
 
